1a4cdb2f58d947086348ab4dddd0173c14db728c
DanielDeanGithub <github-new@inboxrefresh.com>
2025-02-17 07:50:44 +0000

    Add input and label for phone

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -19,6 +19,16 @@ export const ContactForm = ({
         onChange={(e) => setName(e.target.value)}
         required
       />
+      
+      <label htmlFor="phone">Phone:</label>
+      <input
+        id="phone"
+        type="tel"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$"
+        required
+      />
 
     </form>
   );
