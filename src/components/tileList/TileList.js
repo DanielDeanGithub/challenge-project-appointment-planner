ae3399705fcaeb47bc9c778723dca4879b59c191
DanielDeanGithub <github-new@inboxrefresh.com>
2025-02-19 07:55:10 +0000

    Use array passed via props to iteratively render Tile components

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Tile from './Tile';
+import Tile from '../tile/Tile';
 
-export const TileList = ({ contacts }) => {
+export const TileList = ({ items }) => {
   return (
     <div>
-
+      { 
+        items.map((item, index) => {
+          const { name, ...description } = item;
+          return <Tile key={index} name={name} description={description} />;
+        })
+      }
     </div>
   );
 };
