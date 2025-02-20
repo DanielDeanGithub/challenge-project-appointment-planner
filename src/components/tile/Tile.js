9b8132f7f02c4336d419010d175a753c204f9d58
DanielDeanGithub <github-new@inboxrefresh.com>
2025-02-20 07:22:09 +0000

    Render <p> element with name prop

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
     </div>
   );
 };
