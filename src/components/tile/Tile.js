6379b33b7b796e877a710baaa893757f40e52ba4
DanielDeanGithub <github-new@inboxrefresh.com>
2025-02-21 07:37:28 +0000

    Use description passed via props to iteratively render <p> elements

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -4,6 +4,9 @@ export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
       <p className="tile-title">{name}</p>
+      {
+        description.map((value, index) => <p key={index} className="tile">{value}</p>)
+      }
     </div>
   );
 };
